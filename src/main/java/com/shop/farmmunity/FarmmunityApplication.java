d04b00e64a8d88aa1ae734d29a16c115c5f83df7
youngjae459 <youngjae459@gmail.com>
2023-06-21 16:03:20 +0900

    init: 초기 세팅

---
+++ src/main/java/com/shop/farmmunity/FarmmunityApplication.java
@@ -0,0 +1,13 @@
+package com.shop.farmmunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmmunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmmunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/farmmunity/FarmmunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.farmmunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmmunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
