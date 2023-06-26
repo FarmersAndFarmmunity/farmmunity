2c25fa2917c8abb2320c1dea2394a56a7fdfe5c4
whale22 <realwhale64@naver.com>
2023-06-26 14:04:07 +0900

    Member MVC 파일 추가, Base Entity 추가

---
+++ src/main/java/com/shop/farmmunity/domain/member/repository/MemberRepository.java
@@ -0,0 +1,4 @@
+package com.shop.farmmunity.domain.member.repository;
+
+public class MemberRepository {
+}
---
+++ src/main/java/com/shop/farmmunity/domain/member/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.shop.farmmunity.domain.member.service;
+
+import com.shop.farmmunity.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+}
