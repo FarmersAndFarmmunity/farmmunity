3aa4aede3d64eb1ce839bc024415113f8762c350
duswnsxnxn <duswnsxnxn@gmail.com>
2023-07-06 09:46:16 +0900

    refactor: 주문페이지에서 배송지변경 리펙토링

--- src/main/java/com/shop/farmmunity/domain/member/controller/AddressPopupController.java
+++ src/main/java/com/shop/farmmunity/domain/member/controller/AddressPopupController.java
@@ -20,68 +20,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
 import java.util.List;
-
 @Controller
 @RequiredArgsConstructor
-public class AddressController {
+public class AddressPopupController {
 
     private final MemberService memberService;
     private final AddressService addressService;
 
-    @GetMapping(value = "/members/mypage/address")
-    public String addressList(Model model, Principal principal){
-
-        try {
-            Member member = memberService.findByEmail(principal.getName());
-            List<AddressDto> addressList = addressService.getAddressList(member.getId());
-            model.addAttribute("addressList", addressList);
-        } catch (Exception e) {
-            model.addAttribute("errorMessage", e.getMessage());
-            return "member/addressForm";
-        }
-        return "member/addressList";
-    }
-
-    @GetMapping(value = "/members/mypage/address/select")
-    public String selectAddress(Model model, Principal principal){
-
-        try {
-            Member member = memberService.findByEmail(principal.getName());
-            List<AddressDto> addressList = addressService.getAddressList(member.getId());
-            model.addAttribute("addressList", addressList);
-        } catch (Exception e) {
-            model.addAttribute("errorMessage", e.getMessage());
-            return "member/addressForm";
-        }
-        return "member/selectAddress";
-    }
-
-    @GetMapping(value = "/members/mypage/address/select/{selectedId}")
-    public @ResponseBody ResponseEntity selectedAddress(@PathVariable Long selectedId, Model model, Principal principal){
-
-        try {
-            Member member = memberService.findByEmail(principal.getName());
-            Address addressById = addressService.findAddressById(selectedId);
-            if (member.getId() != addressById.getMember().getId()) {
-                return new ResponseEntity<String>("권한이 없습니다.", HttpStatus.BAD_REQUEST);
-            }
-            AddressDto address = addressService.getAddress(selectedId);
-            return new ResponseEntity<AddressDto>(address, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @GetMapping(value = "/members/mypage/address/new")
+    @GetMapping(value = "/members/popup/address/new")
     public String addressForm(Model model){
         model.addAttribute("addressFormDto", new AddressFormDto());
-        return "member/addressForm";
+        return "member/popup/addressForm";
     }
 
-    @PostMapping(value = "/members/mypage/address/new")
+    @PostMapping(value = "/members/popup/address/new")
     public String newAddress(@Valid AddressFormDto addressFormDto, BindingResult bindingResult, Model model, Principal principal) {
         if (bindingResult.hasErrors()) {
-            return "member/addressForm";
+            return "member/popup/addressForm";
         }
 
         try {
@@ -93,13 +48,13 @@ public class AddressController {
             addressService.saveAddress(address);
         } catch (Exception e) {
             model.addAttribute("errorMessage", e.getMessage());
-            return "member/addressForm";
+            return "member/popup/addressForm";
         }
 
-        return "redirect:/members/mypage/address";
+        return "redirect:/members/mypage/address/select";
     }
 
-    @GetMapping(value = "/members/mypage/address/modify/{addressId}")
+    @GetMapping(value = "/members/popup/address/modify/{addressId}")
     public String modifyAddress(@PathVariable Long addressId, Model model, Principal principal) {
 
         try {
@@ -107,18 +62,18 @@ public class AddressController {
             Address addressById = addressService.findAddressById(addressId);
             if (member.getId() != addressById.getMember().getId()) {
                 model.addAttribute("errorMessage", "권한이 없습니다.");
-                return "redirect:/";
+                return "redirect:/members/mypage/address/select";
             }
             AddressDto address = addressService.getAddress(addressId);
             model.addAttribute("addressFormDto", address);
-            return "member/addressForm";
+            return "member/popup/addressForm";
         } catch (Exception e) {
             model.addAttribute("errorMessage", e.getMessage());
-            return "redirect:/members/mypage/address";
+            return "redirect:/members/mypage/address/select";
         }
     }
 
-    @PostMapping(value = "/members/mypage/address/modify/{addressId}")
+    @PostMapping(value = "/members/popup/address/modify/{addressId}")
     public String modifyAddress(@Valid AddressFormDto addressFormDto, BindingResult bindingResult, Model model, Principal principal){
 
         try {
@@ -126,10 +81,10 @@ public class AddressController {
             Address addressById = addressService.findAddressById(addressFormDto.getAddressId());
             if (member.getId() != addressById.getMember().getId()) {
                 model.addAttribute("errorMessage", "권한이 없습니다.");
-                return "redirect:/";
+                return "redirect:/members/mypage/address/select";
             }
             if (bindingResult.hasErrors()) {
-                return "member/addressForm";
+                return "member/popup/addressForm";
             }
             if (addressFormDto.getIs_default()) {
                 addressService.modifyDefaultAddress(member.getId());
@@ -137,13 +92,13 @@ public class AddressController {
             addressService.updateAddress(addressFormDto);
         } catch (Exception e) {
             model.addAttribute("errorMessage", e.getMessage());
-            return "member/addressForm";
+            return "member/popup/addressForm";
         }
 
-        return "redirect:/members/mypage/address";
+        return "redirect:/members/mypage/address/select";
     }
 
-    @GetMapping("/members/mypage/address/delete/{addressId}")
+    @GetMapping("/members/popup/address/delete/{addressId}")
     public String deleteAddress(@PathVariable Long addressId, Principal principal, Model model) {
 
         try {
@@ -151,16 +106,15 @@ public class AddressController {
             Address addressById = addressService.findAddressById(addressId);
             if (member.getId() != addressById.getMember().getId()) {
                 model.addAttribute("errorMessage", "권한이 없습니다.");
-                return "redirect:/";
+                return "redirect:/members/mypage/address/select";
             }
 
             addressService.deleteAddress(addressId);
         } catch (Exception e) {
             model.addAttribute("errorMessage", e.getMessage());
-            return "member/addressForm";
+            return "member/popup/addressForm";
         }
 
-        return "redirect:/members/mypage/address";
+        return "redirect:/members/mypage/address/select";
     }
-
 }
