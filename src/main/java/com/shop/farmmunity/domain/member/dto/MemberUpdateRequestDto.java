7442fe64994f8323478cae71eb021ba624007f75
youngjae459 <youngjae459@gmail.com>
2023-07-10 01:20:15 +0900

    feat: 회원 정보 수정을 위한 dto 및 엔티티 메서드 추가

--- src/main/java/com/shop/farmmunity/domain/member/dto/MemberUpdateRequestDto.java
+++ src/main/java/com/shop/farmmunity/domain/member/dto/MemberUpdateRequestDto.java
@@ -1,5 +1,7 @@
 package com.shop.farmmunity.domain.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +12,19 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberUpdateRequestDto {
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{4,20}$", message = "아이디는 특수문자를 제외한 4~20자리여야 합니다.")
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
     private String username;
+
+    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
+
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
+
+
     private String address;
+
     private LocalDateTime latestUpdateTime;
 }
