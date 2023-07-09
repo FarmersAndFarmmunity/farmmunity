e8f1695b3e7aea9c52b89baa88db8071b2395425
youngjae459 <youngjae459@gmail.com>
2023-07-09 17:28:57 +0900

    feat: 회원정보 수정 위한 dto 생성

---
+++ src/main/java/com/shop/farmmunity/domain/member/dto/MemberUpdateDto.java
@@ -0,0 +1,18 @@
+package com.shop.farmmunity.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberUpdateDto {
+    private String username;
+    private String email;
+    private String password;
+    private String address;
+    private LocalDateTime latestUpdateTime;
+}
